98d290abdbedc1ef5c1dbd7fbe14727b34ca8b3d
Papai145 <mrruslan28@gmail.com>
2024-07-08 21:30:35 +0500

    добавил проверку

--- 2-dice/app.js
+++ 2-dice/app.js
@@ -1,6 +1,12 @@
 "use strict";
 function dice(diceNumber) {
   const diceNum = Number(diceNumber.slice(1));
+  const letter = diceNumber[0];
+
+  if (letter !== "d" || diceNum < 4 || diceNum > 20 || diceNum % 2 !== 0) {
+    console.log("неверный формат");
+    return false;
+  }
   const num = Math.random();
   return Math.floor(num * diceNum + 1);
 }
