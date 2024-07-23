ad5e76e263908e4ab649636a54c6791fe8799754
Papai145 <mrruslan28@gmail.com>
2024-07-23 21:02:26 +0500

    прошу проверить.если не верно скажите без решения

--- 4-timer/app.js
+++ 4-timer/app.js
@@ -12,24 +12,25 @@ const cerrentYaer = new Date().getFullYear();
 const nextYear = new Date(`January 01 ${cerrentYaer + 1} 00:00:00`);
 
 function updateTime() {
-  const currentTime = new Date();
+  setInterval(() => {
+    const currentTime = new Date();
 
-  // const diff = nextYear - currentTime;
-  const diff =
-    new Date(currentTime.getFullYear(), currentTime.getMonth() + 1, 1) -
-    currentTime;
+    // const diff = nextYear - currentTime;
+    const diff =
+      new Date(currentTime.getFullYear(), currentTime.getMonth() + 1, 1) -
+      currentTime;
 
-  const menthLeft = 11 - currentTime.getMonth();
-  const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
-  const secondLeft = Math.floor(diff / 1000) % 60;
+    const menthLeft = 11 - currentTime.getMonth();
+    const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+    const secondLeft = Math.floor(diff / 1000) % 60;
 
-  page.menth.innerText = menthLeft;
-  page.days.innerText = daysLeft;
-  page.hours.innerHTML = hoursLeft < 10 ? "0" + hoursLeft : hoursLeft;
-  page.minutes.innerHTML = minutesLeft < 10 ? "0" + minutesLeft : minutesLeft;
-  page.seconds.innerHTML = secondLeft < 10 ? "0" + secondLeft : secondLeft;
+    page.menth.innerText = menthLeft;
+    page.days.innerText = daysLeft;
+    page.hours.innerHTML = hoursLeft < 10 ? "0" + hoursLeft : hoursLeft;
+    page.minutes.innerHTML = minutesLeft < 10 ? "0" + minutesLeft : minutesLeft;
+    page.seconds.innerHTML = secondLeft < 10 ? "0" + secondLeft : secondLeft;
+  }, 1000);
 }
 updateTime();
-setInterval(updateTime, 1000);
