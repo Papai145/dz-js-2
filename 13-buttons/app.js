26485776ba1e737494b0754f4c4edecf83a412d2
Papai145 <mrruslan28@gmail.com>
2024-07-30 19:30:04 +0500

    Прошу проверить

---
+++ 13-buttons/app.js
@@ -0,0 +1,13 @@
+const parent = document.querySelector(".container");
+const count = document.getElementById("count");
+
+parent.addEventListener("click", function (e) {
+  for (element of [...parent.children]) {
+    if (element.id == e.target.id) {
+      element.innerText = "Нажата";
+      count.innerText = Number(count.textContent) + 1;
+      continue;
+    }
+    element.innerText = "Нажми меня";
+  }
+});
